185c17d3ad0d4a557d83a0b3f484b25ed9c91e06
Chengyu HAN <cyhan@mail.buct.edu.cn>
2023-08-15 10:14:22 +0800

    cpu-tests: string, add more tests for strcpy, memset

--- tests/cpu-tests/tests/string.c
+++ tests/cpu-tests/tests/string.c
@@ -13,14 +13,32 @@ char str1[] = "Hello";
 char str[20];
 
 int main() {
+// -- strcmp
 	check(strcmp(s[0], s[2]) == 0);
 	check(strcmp(s[0], s[1]) < 0);
 	check(strcmp(s[0] + 1, s[1] + 1) < 0);
 	check(strcmp(s[0] + 2, s[1] + 2) < 0);
 	check(strcmp(s[0] + 3, s[1] + 3) < 0);
 
+// -- strcpy
+  strcpy(str, s[5]);
+  check(strcmp(str, s[5]) == 0);
+
+// -- strcat
 	check(strcmp( strcat(strcpy(str, str1), s[3]), s[4]) == 0);
 
+// -- memset
+  memset(str, '\0', 20);
+  check(str[0] == 0);
+  check(str[5] == 0);
+  check(str[6] == 0);
+  memset(str, '#', 5);
+  check(str[0] == '#');
+  check(str[4] == '#');
+  check(str[5] == 0);
+  check(strcmp(str, s[5]) == 0);
+
+// -- memcmp
 	check(memcmp(memset(str, '#', 5), s[5], 5) == 0);
 
 	return 0;
