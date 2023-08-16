12048416efa1b66f9fab7bd3cf9eac13ca54692c
Chengyu HAN <cyhan@mail.buct.edu.cn>
2023-08-16 15:23:03 +0800

    klib: +test_sprintf_fmt_d

--- tests/klib-tests/tests/r-sprintf.c
+++ tests/klib-tests/tests/r-sprintf.c
@@ -1,4 +1,6 @@
+#include <limits.h>
 #include "test-globals.cc"
+
 
 static char outbuf[512] = {0};
 
@@ -39,9 +41,41 @@ void test_sprintf_const() {
 #undef SPRINTF_fmt_S_TEST
 } /* test_sprintf_const */
 
+
+
+void test_sprintf_fmt_d() {
+    clear_buf();
+
+#define SPRINTF_fmt_d_TEST(_const_str, _num) \
+  sprintf(outbuf, "%d", _num); \
+  assert_equals_with_ctx( \
+    0, strcmp(outbuf, _const_str), \
+    printf("\toutbuf = '%s'\n" \
+           "\tconst  = '%s'\n", \
+            outbuf, _const_str));
+// -----------------------------------------------------
+
+  SPRINTF_fmt_d_TEST("0", 0);
+  SPRINTF_fmt_d_TEST("0", -0);
+  SPRINTF_fmt_d_TEST("0", 000);
+  SPRINTF_fmt_d_TEST("1", 1);
+  SPRINTF_fmt_d_TEST("256", 256);
+
+  SPRINTF_fmt_d_TEST("-2147483648", INT_MIN);
+  SPRINTF_fmt_d_TEST("-2147483647", INT_MIN + 1);
+  SPRINTF_fmt_d_TEST("126322567",   INT_MAX / 17);
+  SPRINTF_fmt_d_TEST("252645135",   UINT_MAX / 17);
+  SPRINTF_fmt_d_TEST("2147483647",  INT_MAX);
+  SPRINTF_fmt_d_TEST("-1",  UINT_MAX);
+  SPRINTF_fmt_d_TEST("0",   UINT_MAX+1);
+
+#undef SPRINTF_fmt_d_TEST
+} // test_sprintf_fmt_d
+
 int main() {
 
   test_sprintf_const();
+  test_sprintf_fmt_d();
 
 	return 0;
 }
