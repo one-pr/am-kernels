5230b7c43657bac1206562fd06a90af308de7fd0
Chengyu HAN <cyhan@mail.buct.edu.cn>
2023-08-16 13:18:59 +0800

    klib: use assert_equals

--- tests/klib-tests/tests/mr-strlen.c
+++ tests/klib-tests/tests/mr-strlen.c
@@ -12,46 +12,46 @@ static char zeros_carr[8] = { 0 };
 // 简单的手动测试
 void test_strlen_trivial() {
 // const string
-  assert(0 == strlen(""));
+  assert_equals(0, strlen(""));
 
-  assert(1 == strlen("0"));
-  assert(1 == strlen("1"));
-  assert(2 == strlen("10"));
-  assert(2 == strlen("()"));
-  assert(2 == strlen("[]"));
-  assert(2 == strlen("{}"));
-  assert(2 == strlen("<>"));
-  assert(2 == strlen("''"));
-  assert(2 == strlen("\"\""));
+  assert_equals(1, strlen("0"));
+  assert_equals(1, strlen("1"));
+  assert_equals(2, strlen("10"));
+  assert_equals(2, strlen("()"));
+  assert_equals(2, strlen("[]"));
+  assert_equals(2, strlen("{}"));
+  assert_equals(2, strlen("<>"));
+  assert_equals(2, strlen("''"));
+  assert_equals(2, strlen("\"\""));
 
-  assert(4 == strlen("ysyx"));
-  assert(5 == strlen("cyhan"));
-  assert(6 == strlen("strlen"));
-  assert(8 == strlen("01234567"));
-  assert(10 == strlen("0123456789"));
-  assert(10 == strlen("!@#$%^&*()"));  // <Shift> + <0-9>
-  assert(10 == strlen("1234567890"));
-  assert(12 == strlen("Hello World!"));
-  assert(16 == strlen("0123456789ABCDEF"));
-  assert(16 == strlen("0123456789abcdef"));
-  assert(26 == strlen("ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
-  assert(26 == strlen("abcdefghijklmnopqrstuvwxyz"));
+  assert_equals(4, strlen("ysyx"));
+  assert_equals(5, strlen("cyhan"));
+  assert_equals(6, strlen("strlen"));
+  assert_equals(8, strlen("01234567"));
+  assert_equals(10, strlen("0123456789"));
+  assert_equals(10, strlen("!@#$%^&*()"));  // <Shift> + <0-9>
+  assert_equals(10, strlen("1234567890"));
+  assert_equals(12, strlen("Hello World!"));
+  assert_equals(16, strlen("0123456789ABCDEF"));
+  assert_equals(16, strlen("0123456789abcdef"));
+  assert_equals(26, strlen("ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
+  assert_equals(26, strlen("abcdefghijklmnopqrstuvwxyz"));
 
 // empty str
-  assert(0 == strlen(""));
-  assert(0 == strlen(empty_str));
-  assert(0 == strlen(empty_strarr[0]));
-  assert(0 == strlen(empty_carr));
-  assert(0 == strlen(empty_null));
-  assert(0 == strlen(zeros_carr));
+  assert_equals(0, strlen(""));
+  assert_equals(0, strlen(empty_str));
+  assert_equals(0, strlen(empty_strarr[0]));
+  assert_equals(0, strlen(empty_carr));
+  assert_equals(0, strlen(empty_null));
+  assert_equals(0, strlen(zeros_carr));
 
 // 常量字符串
-  assert(ascii_char_len == strlen(ascii_chars));
-  assert(special_chars_len == strlen(special_chars));
-  assert(numner_chars_len == strlen(numner_chars));
-  assert(uppercase_chars_len == strlen(uppercase_chars));
-  assert(lowercase_chars_len == strlen(lowercase_chars));
-  assert(simple_escape_chars_len == strlen(simple_escape_chars));
+  assert_equals(ascii_char_len, strlen(ascii_chars));
+  assert_equals(special_chars_len, strlen(special_chars));
+  assert_equals(numner_chars_len, strlen(numner_chars));
+  assert_equals(uppercase_chars_len, strlen(uppercase_chars));
+  assert_equals(lowercase_chars_len, strlen(lowercase_chars));
+  assert_equals(simple_escape_chars_len, strlen(simple_escape_chars));
 } /* test_strlen_trivial */
 
 // 无其他依赖的自动化测试
