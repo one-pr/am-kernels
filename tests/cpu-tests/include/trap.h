bf42d04b1edcd3674819710a3f7e98f863ae0674
Chengyu HAN <cyhan@mail.buct.edu.cn>
2023-09-21 21:45:23 +0800

    am.add: 格式化数组

--- tests/cpu-tests/include/trap.h
+++ tests/cpu-tests/include/trap.h
@@ -5,14 +5,18 @@
 #include <klib.h>
 #include <klib-macros.h>
 
-__attribute__((noinline))
+// 标记函数为非内联
+#define __FUNC_NO_INLINE    __attribute__((noinline))
+
+
+__FUNC_NO_INLINE
 void check(bool cond) {
   if (!cond) halt(1);
 }
 
-__attribute__((noinline))
+__FUNC_NO_INLINE
 void check_ctx(bool cond, int ctx) {
   if (!cond) halt(ctx);
 }
 
-#endif
+#endif // !__TRAP_H__
