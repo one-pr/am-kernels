73995b46e203227b9f83f08a40785cd05fbd69e1
Chengyu HAN <cyhan@mail.buct.edu.cn>
2023-09-21 20:51:24 +0800

    am: 重构 crc32

--- tests/cpu-tests/tests/crc32.c
+++ tests/cpu-tests/tests/crc32.c
@@ -3,45 +3,49 @@
 #include <klib-macros.h>
 #include "trap.h"
 
+#define CRC_PLOY_SIZE   256
 #define STR "The quick brown fox jumps over the lazy dog"
 #define STRLEN (sizeof(STR) - 1)
- 
-uint32_t rc_crc32(uint32_t crc, const char *buf, size_t len) {
-	static uint32_t table[256];
-	static int have_table = 0;
-	uint32_t rem;
-	uint8_t octet;
-	int i, j;
-	const char *p, *q;
- 
-	/* This check is not thread safe; there is no mutex. */
-	if (have_table == 0) {
-		/* Calculate CRC table. */
-		for (i = 0; i < 256; i++) {
-			rem = i;  /* remainder from polynomial division */
-			for (j = 0; j < 8; j++) {
-				if (rem & 1) {
-					rem >>= 1;
-					rem ^= 0xedb88320;
-				} else
-					rem >>= 1;
-			}
-			table[i] = rem;
-		}
-		have_table = 1;
-	}
- 
+
+static uint32_t g_crc32_table[CRC_PLOY_SIZE];
+
+
+/* Calculate CRC table. */
+static void crc32_init() {
+  for (int i = 0; i < CRC_PLOY_SIZE; i++) {
+    /* remainder from polynomial division */
+    uint32_t rem = i;
+
+    for (int j = 0; j < 8; j++) {
+      if (rem & 1) {
+        rem >>= 1;
+        rem ^= 0xedb88320;
+      } else {
+        rem >>= 1;
+      }
+    }
+
+    g_crc32_table[i] = rem;
+  }
+} /* crc32_init */
+
+uint32_t crc32(uint32_t crc, const char *buf, size_t len) {
 	crc = ~crc;
-	q = buf + len;
-	for (p = buf; p < q; p++) {
-		octet = *p;  /* Cast to unsigned octet. */
-		crc = (crc >> 8) ^ table[(crc & 0xff) ^ octet];
+
+	const char *end = buf + len;
+	for (const char *p = buf; p < end; p++) {
+		uint8_t octet = *p;  /* Cast to unsigned octet. */
+		crc = (crc >> 8) ^ g_crc32_table[(crc & 0xff) ^ octet];
 	}
+
 	return ~crc;
-}
- 
+} /* crc32 */
+
+/* This check is not thread safe; there is no mutex. */
 int main() {
-  uint32_t res = rc_crc32(0, STR, STRLEN);
-  check(res == 0x414FA339);
+  crc32_init();
+
+  check(0x414FA339 == crc32(0, STR, STRLEN));
+
 	return 0;
 }
