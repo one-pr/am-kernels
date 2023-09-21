01ed552a13691bd88b30067583ffdd5a78641cfa
Chengyu HAN <cyhan@mail.buct.edu.cn>
2023-09-21 21:22:30 +0800

    am.crc32: add more tests

--- tests/cpu-tests/tests/crc32.c
+++ tests/cpu-tests/tests/crc32.c
@@ -4,10 +4,43 @@
 #include "trap.h"
 
 #define CRC_PLOY_SIZE   256
-#define STR "The quick brown fox jumps over the lazy dog"
-#define STRLEN (sizeof(STR) - 1)
+#define STRLEN(_str)      (sizeof(_str) - 1)
+#define STR_ARGS(_str)    _str, STRLEN(_str)
 
 static uint32_t g_crc32_table[CRC_PLOY_SIZE];
+static struct test {
+  uint32_t crc;
+  const char *str;
+  uint32_t len;
+} tests[] = {
+  {0x414FA339, STR_ARGS("The quick brown fox jumps over the lazy dog")},
+
+  {0x00000000, STR_ARGS("")},
+  {0xE96CCF45, STR_ARGS(" ")},
+  {0xE8B7BE43, STR_ARGS("a")},
+  {0x352441C2, STR_ARGS("abc")},
+  {0xAFABD35E, STR_ARGS("crc32")},
+  {0xF52FB817, STR_ARGS("Risc-V 32")},
+  {0x1B851995, STR_ARGS("Hello world!")},
+  {0xAAB34F5B, STR_ARGS("Cyclic Redundancy Check")},
+  {0x9CD969CA, STR_ARGS("Welcome to riscv32-NEMU!")},
+
+  {0xABF77822, STR_ARGS("ABCDEFGHIJKLMNOPQRSTUVWXYZ")},
+  {0x4C2750BD, STR_ARGS("abcdefghijklmnopqrstuvwxyz")},
+  {0x1fc2e6d2, STR_ARGS(
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    "abcdefghijklmnopqrstuvwxyz"
+    "0123456789")},
+  {0x9CD969CA, STR_ARGS("Welcome to riscv32-NEMU!")},
+  {0x342A13F2, STR_ARGS(
+    "If trace is enabled,"
+    " a log file will be generated to record the trace."
+    " This may lead to a large log file."
+    " If it is not necessary,"
+    " you can disable it in menuconfig")},
+
+  {0, NULL, 0},
+}; // tests[]
 
 
 /* Calculate CRC table. */
@@ -45,7 +78,13 @@ uint32_t crc32(uint32_t crc, const char *buf, size_t len) {
 int main() {
   crc32_init();
 
-  check(0x414FA339 == crc32(0, STR, STRLEN));
+  for (int i=0; tests[i].str; i++) {
+    uint32_t crc = tests[i].crc;
+    const char *str = tests[i].str;
+    uint32_t len = tests[i].len;
+
+    check(crc == crc32(0, str, len));
+  }
 
 	return 0;
 }
